ac74b821f0e22faa38308896b3a822bbbf505bd1
Faesposito <49763803+Faesposito@users.noreply.github.com>
2020-07-07 19:41:08 -0300

    Add files via upload

---
+++ Factory/Factory.cpp
@@ -0,0 +1,66 @@
+/************** Factory Design Pattern *************
+ *  Franco Esposito.
+ *  Edi III.
+ ***************************************************/
+
+#include <iostream>
+
+class Computer
+{
+public:
+    virtual ~Computer(){};
+    virtual void Run() = 0;
+    virtual void Stop() = 0;
+};
+
+class Laptop : public Computer
+{
+public:
+    virtual ~Laptop(){};
+    void Run() { std::cout << "Laptop Is Running" << std::endl; };
+    void Stop() { std::cout << "Laptop has Stopped" << std::endl; };
+};
+
+class Desktop : public Computer
+{
+public:
+    virtual ~Desktop(){};
+    void Run() { std::cout << "Desktop Is Running" << std::endl; };
+    void Stop() { std::cout << "Desktop has Stopped" << std::endl; };
+};
+
+class ComputerFactory
+{
+public:
+    static Computer *NewComputer(const std::string &description)
+    {
+        if (description == "laptop")
+            return new Laptop;
+        if (description == "desktop")
+            return new Desktop;
+        return nullptr;
+    }
+    void doSomething(const std::string &description)
+    {
+        Computer *computer = this->NewComputer(description);
+        computer->Run();
+        computer->Stop();
+    }
+};
+
+void userChoice(ComputerFactory &computerFactory)
+{
+    std::string description;
+    std::cout << "select type of computer: ";
+    std::cin >> description;
+    computerFactory.doSomething(description);
+}
+
+int main()
+{
+    ComputerFactory *computerFactory = new ComputerFactory();
+    userChoice(*computerFactory);
+    userChoice(*computerFactory);
+
+    return 0;
+}
